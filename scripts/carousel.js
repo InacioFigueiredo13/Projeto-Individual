c81bba3b3032b932dbc8f9191d34d2a8af9a26a3
InacioFigueiredo13 <inaciofigueiredo13@gmail.com>
2023-05-20 16:58:34 -0300

    Adicionando home carousel

---
+++ scripts/carousel.js
@@ -0,0 +1,15 @@
+let count = 1;
+document.getElementById('radio1').checked = true;
+
+// setInterval(() => {
+//     autoPassImage();
+// }, 10000)
+
+function autoPassImage() {
+    count++;
+    if (count > 4) {
+        count = 1;
+    }
+    document.getElementById('radio' + count).checked = true;
+    
+}
