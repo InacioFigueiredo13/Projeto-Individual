40a01326b94c1c66a8648c8503550972f0d0eeb1
InacioFigueiredo13 <inaciofigueiredo13@gmail.com>
2023-05-27 12:16:35 -0300

    Carousel de eventos finalizado

---
+++ scripts/carousel_agenda.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const carousel = document.querySelector(".carousel-slides");
+    const carouselItems = document.querySelectorAll(".evento-card-container");
+    const itemWidth = carouselItems[0].offsetWidth;
+
+    let position = 0;
+
+    function moveCarousel(direction) {
+        const itemsToShow = Math.floor(carousel.clientWidth / itemWidth);
+
+        if (direction === "next" && position > -(carouselItems.length - itemsToShow) * itemWidth) {
+            position -= itemWidth;
+        } else if (direction === "prev" && position < 0) {
+            position += itemWidth;
+        }
+
+        carousel.style.transform = `translateX(${position}px)`;
+    }
+
+    document.querySelector("#carousel-prev").addEventListener("click", function () {
+        moveCarousel("prev");
+    });
+
+    document.querySelector("#carousel-next").addEventListener("click", function () {
+        moveCarousel("next");
+    });
+});
